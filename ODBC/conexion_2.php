a40d3bb2ab77f10c474ed9991e8aa2591209f3c6
PabloSamanoElton <44504763+PabloSamanoElton@users.noreply.github.com>
2019-05-14 11:05:04 -0500

    conexion_2
    Final de conexion_2

--- ODBC/conexion_2.php
+++ ODBC/conexion_2.php
@@ -1,1 +1,15 @@
-conexion_2.php
+<?php 
+	$dsn="prueba";
+	$usuario="";
+	$password="";
+
+	$conexion = odbc_connect($dsn, $usuario, $password);
+
+	$SQL = "SELECT * FROM alumnos;";
+	$Consulta = odbc_exec($conexion, $SQL);
+
+	$resultado = odbc_result_all($consulta, "border = 1");
+	print($resultado);
+
+	odbc_close($conexion);
+?>
