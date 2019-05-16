9627a2cbe0cfdfa35bd9996709a0ff6fbb22ab26
PabloSamanoElton <44504763+PabloSamanoElton@users.noreply.github.com>
2019-05-16 14:22:01 -0500

    idk

--- templates/forms/PLicencias.php
+++ templates/forms/PLicencias.php
@@ -10,11 +10,19 @@ if(isset($_POST['Submit'])){
     $Expide= $_POST['expide'];
     $Foto= $_FILES['Foto'];
 
+    $_FILES['Foto']['name']=$Conductor . ".jpg";
+    
+    $name = $_FILES['Foto']['name'];
+    $location = "C:/xampp/htdocs/ControlVehicular/templates/img/Fotos";
+    $tmp_name = $_FILES['Foto']['tmp_name'];
     
 
+    move_uploaded_file($tmp_name, $location.$name);
+    $location2=$location.$Conductor;
+
     $Con = Conectar();
-    $SQL = "INSERT INTO licencias(Conductor, Expedicion, Tipo, Vencimiento, Lugar, Expide) 
-        VALUES ('$Conductor', '$fExpedicion', '$Tipo', '$fVencimiento', '$Lugar', '$Expide')";
+    $SQL = "INSERT INTO licencias(Conductor, Expedicion, Tipo, Vencimiento, Lugar, Expide, Foto) 
+        VALUES ('$Conductor', '$fExpedicion', '$Tipo', '$fVencimiento', '$Lugar', '$Expide','$location2')";
     EjecutarConsulta($Con,$SQL);
     Desconectar($Con);
 }
