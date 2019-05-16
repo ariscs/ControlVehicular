3e1d407e39d98177da1a080af94788a1af99c141
PabloSamanoElton <44504763+PabloSamanoElton@users.noreply.github.com>
2019-05-16 16:34:49 -0500

    key

--- functions/access.php
+++ functions/access.php
@@ -4,6 +4,9 @@ session_start();
 
 $user = $_POST['username'];
 $psw = $_POST['psw'];
+$key = $_FILES['key'];
+
+$ubicacionK = $key['tmp_name'];
 
 include('conexion.php');
 $Con = Conectar();
@@ -18,13 +21,25 @@ if($n == 0){
 	$fila = mysqli_fetch_row($Query);
 	if($fila[2] == 1){
 		if($fila[1] == $psw){
-			//Entra correctamente
-			$SQL = "UPDATE usuarios SET Intento = 0 WHERE Username = '$user'";
-			EjecutarConsulta($Con, $SQL);
-			$_SESSION['username'] = $user;
-			$_SESSION['val'] = TRUE;
-			$_SESSION['time'] = time();
-			header("Location:../index.php?view=".urlencode("menu"));
+			$string="";
+			$data = fopen($ubicacionK,"r");
+			while(!feof($data)){
+				$s1 = fgets($data);
+				$string = $string.$s1;
+			}
+			if ($fila[4]==$string) {
+				//Entra correctamente
+				$SQL = "UPDATE usuarios SET Intento = 0 WHERE Username = '$user'";
+				EjecutarConsulta($Con, $SQL);
+				$_SESSION['username'] = $user;
+				$_SESSION['val'] = TRUE;
+				$_SESSION['time'] = time();
+				header("Location:../index.php?view=".urlencode("menu"));
+			}else{
+				header("Location:../index.php?view=".urlencode("login"));
+			}
+			fclose($data);
+			
 
 		}else{
 			//Contrasena incorrecta
